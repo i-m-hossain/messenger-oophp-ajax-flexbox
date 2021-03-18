3fa6d85cb073c1f88e2ad906613dbff083c7481f
Md. Imran Hossain <mdimran1409036@gmail.com>
2021-03-19 04:32:13 +0600

    emoji added

--- components/js.php
+++ components/js.php
@@ -1,13 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/emojionearea.min.js"></script>
-
-<script type="text/javascript">
-    $(document).ready(function() {
-        $("#example1").emojioneArea({
-            pickerPosition: "bottom"
-        });
-    });
-</script>
 <script type="text/javascript">
     $(document).ready(function() {
         $(".custom-bars").click(function() {
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php 
-    include "classes/init.php";
+<?php
+include "classes/init.php";
 
-    if(!isset($_SESSION['user_id'])){
+if (!isset($_SESSION['user_id'])) {
 
-        header("location:login.php");
-    }
+    header("location:login.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -27,16 +27,38 @@
     <div class="chat-container">
 
         <?php include("components/sidebar.php") ?>
-        
+
         <section id="right-area">
             <?php include "components/messages.php" ?>
 
             <?php include "components/chat-form.php" ?>
+            <div class="emoji">
+                <img src="assets/emoji/001-happy-18.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/002-cool-5.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/004-surprised-9.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/005-shocked-4.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/007-nervous-2.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/009-angry-6.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/010-drool.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/011-tired-2.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/012-tongue-7.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/015-smile-1.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/016-sleeping-1.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/021-wink-1.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/022-laughing-2.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/024-sweat-1.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/031-crying-7.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/032-bored.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/034-angry-5.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/039-cyclops-1.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/042-book.svg" alt="" class="emoji-same">
+                <img src="assets/emoji/044-dead-1.svg" alt="" class="emoji-same">
 
-
+            </div><!--close emoji-->
         </section><!--close right-area-->
-    </div><!--close chat-container -->
-    <?php include "components/js.php"?>
+    </div>
+    <!--close chat-container -->
+    <?php include "components/js.php" ?>
 
 
 </body>
